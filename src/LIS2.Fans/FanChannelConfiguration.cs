897e49d0b39a10181a4918d4674d13b089e89756
Eitschman <eitschman@nebelreich.de>
2026-09-23 11:23:36 +0200

    feat(fans): add channel configuration

---
+++ src/LIS2.Fans/FanChannelConfiguration.cs
@@ -0,0 +1,12 @@
+namespace LIS2.Fans;
+
+public sealed class FanChannelConfiguration
+{
+    public FanMode Mode { get; set; } = FanMode.Fixed;
+    public int FixedPercent { get; set; } = 100;
+    public int MinimumPercent { get; set; } = 30;
+    public int MaximumPercent { get; set; } = 100;
+    public int FailSafePercent { get; set; } = 100;
+    public bool AllowStop { get; set; }
+    public List<FanCurvePoint> Curve { get; set; } = new();
+}
