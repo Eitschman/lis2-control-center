8402b19d15ba1c3491e5cac92d5f51ac4581b48b
Eitschman <eitschman@nebelreich.de>
2026-09-23 11:52:18 +0200

    feat(winamp-native): add minimal verified Winamp ABI

---
+++ native/LIS2.WinampPlugin/winamp_sdk_min.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <windows.h>
+#include <cstddef>
+
+struct winampGeneralPurposePlugin
+{
+    int version;
+    char* description;
+    int (*init)();
+    void (*config)();
+    void (*quit)();
+    HWND hwndParent;
+    HINSTANCE hDllInstance;
+};
+
+constexpr int GPPHDR_VER = 0x10;
+
+constexpr UINT WM_WA_IPC = WM_USER;
+
+constexpr LPARAM IPC_ISPLAYING = 104;
+constexpr LPARAM IPC_GETOUTPUTTIME = 105;
+constexpr LPARAM IPC_GETLISTLENGTH = 124;
+constexpr LPARAM IPC_GETLISTPOS = 125;
+constexpr LPARAM IPC_GETINFO = 126;
+
+constexpr LPARAM IPC_GETPLAYLISTFILE = 211;
+constexpr LPARAM IPC_GETPLAYLISTTITLE = 212;
+
+constexpr LPARAM IPC_GET_EXTENDED_FILE_INFO = 290;
+constexpr LPARAM IPC_GET_EXTENDED_FILE_INFO_HOOKABLE = 296;
+
+struct extendedFileInfoStruct
+{
+    const char* filename;
+    const char* metadata;
+    char* ret;
+    std::size_t retlen;
+};
