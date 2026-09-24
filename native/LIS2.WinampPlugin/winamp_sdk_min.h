3e0ecbf8ae615ee24d008969e5fca6e2149f5771
Eitschman <eitschman@nebelreich.de>
2026-09-24 17:00:57 +0200

    feat: add Winamp visualization IPC constants

--- native/LIS2.WinampPlugin/winamp_sdk_min.h
+++ native/LIS2.WinampPlugin/winamp_sdk_min.h
@@ -30,6 +30,9 @@ constexpr LPARAM IPC_GETPLAYLISTTITLE = 212;
 constexpr LPARAM IPC_GET_EXTENDED_FILE_INFO = 290;
 constexpr LPARAM IPC_GET_EXTENDED_FILE_INFO_HOOKABLE = 296;
 
+constexpr LPARAM IPC_GETSADATAFUNC = 800;
+constexpr LPARAM IPC_GETVUDATAFUNC = 801;
+
 struct extendedFileInfoStruct
 {
     const char* filename;
