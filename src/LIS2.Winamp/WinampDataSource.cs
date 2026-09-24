f571d5583cee92bd4d5fa13103a9ffccf685239b
Eitschman <eitschman@nebelreich.de>
2026-09-24 17:35:32 +0200

    feat: initialize raw Winamp spectrum value

--- src/LIS2.Winamp/WinampDataSource.cs
+++ src/LIS2.Winamp/WinampDataSource.cs
@@ -81,6 +81,7 @@ public sealed class WinampDataSource : IDataSource
             ["VuRight"] = null,
             ["Vu"] = string.Empty,
             ["Spectrum"] = string.Empty,
+            ["SpectrumRaw"] = null,
             ["SpectrumPeak"] = null,
             ["Connected"] = false,
             ["SnapshotAgeSeconds"] = null
