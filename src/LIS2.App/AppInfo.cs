8854d07147007243c05df00bfdaaf35fda64744b
Eitschman <eitschman@nebelreich.de>
2026-09-24 18:37:02 +0200

    feat: add application version information

---
+++ src/LIS2.App/AppInfo.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace LIS2.App;
+
+public static class AppInfo
+{
+    public const string RepositoryUrl = "https://github.com/Eitschman/lis2-control-center";
+
+    public static string ProductName => "LIS2 Control Center";
+
+    public static string Version
+    {
+        get
+        {
+            var assembly = typeof(AppInfo).Assembly;
+            var informational = assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion;
+
+            if (!string.IsNullOrWhiteSpace(informational))
+                return informational.Split('+')[0];
+
+            return assembly.GetName().Version?.ToString(3) ?? "unknown";
+        }
+    }
+
+    public static string BuildConfiguration =>
+#if DEBUG
+        "Debug";
+#else
+        "Release";
+#endif
+
+    public static string Runtime =>
+        RuntimeInformation.FrameworkDescription;
+
+    public static string Architecture =>
+        RuntimeInformation.ProcessArchitecture.ToString();
+
+    public static string OperatingSystem =>
+        RuntimeInformation.OSDescription;
+
+    public static string DataDirectory =>
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "LIS2ControlCenter");
+}
